529e9aa54c02c976b1f55d6fc60d65fb5a89a987
C4RLOS-JR <camjr3z@gmail.com>
2024-07-29 19:55:06 -0300

    Carlos

--- perfil/models.py
+++ perfil/models.py
@@ -11,4 +11,21 @@ class Categoria(models.Model):
 
 
 class Conta(models.Model):
+  banco_choices = (
+    ('NU', 'NuBank'),
+    ('CE', 'Caixa Econômica')
+  )
+
+  tipo_choices = (
+    ('pf', 'Pessoa Física'),
+    ('pj', 'Pesssoa Jurídica')
+  )
+
   nome = models.CharField(max_length=50)
+  banco = models.CharField(max_length=2, choices=banco_choices)
+  tipo = models.CharField(max_length=2, choices=tipo_choices)
+  valor = models.FloatField()
+  icone = models.ImageField(upload_to='icones')
+
+  def __str__(self):
+    return self.nome
--- perfil/views.py
+++ perfil/views.py
@@ -6,3 +6,15 @@ def home(request):
 
 def gerenciar(request):
   return render(request, 'gerenciar.html')
+  
+def cadastrar_banco(request):
+  nome = request.POST.get('nome')
+  banco = request.POST.get('banco')
+  tipo = request.POST.get('tipo')
+  valor = request.POST.get('valor')
+  icone = request.FILES.get('icone')
+
+  if (len(nome)==0) or (len(banco)==0) or (len(valor)==0) or (icone==None):
+    return HttpResponse('Preencha todos os campos')
+  
+  return HttpResponse(f'{nome} - {banco} - {tipo} - {valor}')
