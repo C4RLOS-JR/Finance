16505bd50775a12fe3872b4389cb95c004daf663
Leonam-Comercial <leonam.comseg@gmail.com>
2024-07-30 17:25:54 -0300

    Leonam

--- perfil/urls.py
+++ perfil/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('home/', views.home, name='home'),
   path('gerenciar/', views.gerenciar, name='gerenciar'),
-  path('cadastrar_banco', views.cadastrar_banco, name='cadastrar_banco'),
+  path('cadastrar_banco/', views.cadastrar_banco, name='cadastrar_banco'),
+  path('excluir_banco/<int:conta_id>', views.excluir_banco, name='excluir_banco'),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Conta
 
 def home(request):
   return render(request, 'home.html')
 
 def gerenciar(request):
-  return render(request, 'gerenciar.html')
+  contas = Conta.objects.all()
+  return render(request, 'gerenciar.html', {'contas': contas})
   
 def cadastrar_banco(request):
   nome = request.POST.get('nome')
@@ -14,7 +16,27 @@ def cadastrar_banco(request):
   valor = request.POST.get('valor')
   icone = request.FILES.get('icone')
 
-  if (len(nome)==0) or (len(banco)==0) or (len(valor)==0) or (icone==None):
+  if (len(nome)==0) or (len(banco)==0) or (len(valor)==0):
     return HttpResponse('Preencha todos os campos')
-  
-  return HttpResponse(f'{nome} - {banco} - {tipo} - {valor}')
+  if not icone:
+    icone = 'default/icon_bank.png'
+    
+  nova_conta = Conta(
+    nome = nome,
+    banco = banco,
+    tipo = tipo,
+    valor = valor,
+    icone = icone
+  )
+  nova_conta.save()
+
+  return redirect('gerenciar')
+
+def excluir_banco(request, conta_id):
+  conta = Conta.objects.get(id=conta_id)
+
+  if not conta.icone == ('default/icon_bank.png'):
+    conta.icone.delete()
+  conta.delete()
+
+  return redirect('gerenciar')
