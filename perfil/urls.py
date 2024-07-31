7959bf9f587ade4428ff2374fa7c409936948af7
C4RLOS-JR <camjr3z@gmail.com>
2024-07-30 23:39:24 -0300

    Carlos

--- perfil/urls.py
+++ perfil/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('gerenciar/', views.gerenciar, name='gerenciar'),
   path('cadastrar_banco/', views.cadastrar_banco, name='cadastrar_banco'),
   path('excluir_banco/<int:conta_id>', views.excluir_banco, name='excluir_banco'),
+  path('cadastrar_categoria/', views.cadastrar_categoria, name='cadastrar_categoria'),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .models import Conta
+from .models import Conta, Categoria
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def home(request):
   return render(request, 'home.html')
 
 def gerenciar(request):
   contas = Conta.objects.all()
-  return render(request, 'gerenciar.html', {'contas': contas})
+  categorias = Categoria.objects.all()
+  valor_total = 0
+
+  for conta in contas:
+    valor_total += conta.valor
+
+  return render(request, 'gerenciar.html',{
+    'contas': contas,
+    'valor_total': valor_total,
+    'categorias': categorias})
   
 def cadastrar_banco(request):
   nome = request.POST.get('nome')
@@ -16,20 +27,22 @@ def cadastrar_banco(request):
   valor = request.POST.get('valor')
   icone = request.FILES.get('icone')
 
-  if (len(nome)==0) or (len(banco)==0) or (len(valor)==0):
-    return HttpResponse('Preencha todos os campos')
+  if (len(nome.strip()) == 0) or (len(banco) == 0) or (len(valor.strip()) == 0):
+    messages.add_message(request, constants.ERROR, 'Preencha todos os campos!')
+    return redirect('gerenciar')
   if not icone:
     icone = 'default/icon_bank.png'
     
-  nova_conta = Conta(
+  conta = Conta(
     nome = nome,
     banco = banco,
     tipo = tipo,
     valor = valor,
     icone = icone
   )
-  nova_conta.save()
+  conta.save()
 
+  messages.add_message(request, constants.SUCCESS, 'Conta adicionada com sucesso!')
   return redirect('gerenciar')
 
 def excluir_banco(request, conta_id):
@@ -39,4 +52,20 @@ def excluir_banco(request, conta_id):
     conta.icone.delete()
   conta.delete()
 
+  messages.add_message(request, constants.SUCCESS, 'Conta excluída com sucesso!')
+  return redirect('gerenciar')
+
+def cadastrar_categoria(request):
+  categoria = request.POST.get('categoria')
+  essencial = bool(request.POST.get('essencial'))
+
+  # Fazer  as validações:
+
+  nova_categoria = Categoria(
+    categoria = categoria,
+    essencial = essencial
+  )
+  nova_categoria.save()
+
+  messages.add_message(request, constants.SUCCESS, 'Categoria adicionada com sucesso!')
   return redirect('gerenciar')
