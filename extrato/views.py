e535a13c1844fb217438c7659e4bdc02e3e8f2ff
C4RLOS-JR <camjr3z@gmail.com>
2024-08-04 22:07:35 -0300

    Carlos

--- extrato/views.py
+++ extrato/views.py
@@ -6,41 +6,51 @@ from django.contrib.messages import constants
 from django.contrib import messages
 
 def novo_valor(request):
-  contas = Conta.objects.all()
-  categorias = Categoria.objects.all()
   if request.method == 'GET':
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
     return render(request, 'novo_valor.html', {'contas': contas, 'categorias': categorias})
   
-  elif request.method == 'POST':
+  if request.method == 'POST':
     valor = request.POST.get('valor')
     categoria = request.POST.get('categoria')
     descricao = request.POST.get('descricao')
     data = request.POST.get('data')
-    conta = request.POST.get('conta')
+    id_conta = request.POST.get('id_conta')
     tipo = request.POST.get('tipo')
+    valor = valor.replace(',', '.')
 
     if (len(valor.strip())==0) or (len(descricao.strip())==0) or (len(data)==0) or (len(tipo)==0):
       messages.add_message(request, constants.ERROR, 'Preencha todos os campos!')
       return redirect('novo_valor')
     
-    if not valor.isnumeric():
-      messages.add_message(request, constants.ERROR, 'O valor precisa ser um número!')
-      return redirect('novo_valor')
+    # if not valor.isnumeric():
+    #   messages.add_message(request, constants.ERROR, 'O valor precisa ser um número!')
+    #   return redirect('novo_valor')
+
 
     try:
+      conta = Conta.objects.get(id=id_conta)
+      
       novo_valor = Valores(
         valor = valor,
         categoria_id = categoria,
         descricao = descricao,
         data = data,
-        conta_id = conta,
+        conta_id = id_conta,
         tipo = tipo)
       novo_valor.save()
 
       if tipo == 'S':
+        if conta.valor - float(valor) < 0:
+          messages.add_message(request, constants.ERROR, 'Essa conta não tem saldo suficiente para essa despesa...escolha outra conta!')
+          return redirect('novo_valor')
+        conta.valor -= float(valor)
         messages.add_message(request, constants.SUCCESS, 'Saída cadastrada com sucesso!')
       else:
+        conta.valor += float(valor)
         messages.add_message(request, constants.SUCCESS, 'Entrada cadastrada com sucesso!')
+      conta.save()
       return redirect('novo_valor')
     except:
       messages.add_message(request, constants.ERROR, 'Algo deu errado...tente novamente ou fale com um administrador!')
