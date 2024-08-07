6f5a3361518b23f02fb2005f0ace0286e199593a
C4RLOS-JR <camjr3z@gmail.com>
2024-08-06 21:40:02 -0300

    Carlos

--- planejamento/urls.py
+++ planejamento/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('definir_planejamento/', views.definir_planejamento, name='definir_planejamento'),
+    path('modificar_valor_planejamento/<int:id>', views.modificar_valor_planejamento, name='modificar_valor_planejamento'),
 ]
--- planejamento/views.py
+++ planejamento/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from contas.models import Categoria
 
-# Create your views here.
+def definir_planejamento(request):
+  categorias = Categoria.objects.all()
+  return render(request, 'definir_planejamento.html', {'categorias': categorias})
+
+def modificar_valor_planejamento(request, id):
+  categoria = Categoria.objects.get(id=id)
+
+  return HttpResponse(categoria)
