88f7a558abc3d36030d59fcf61eff1bfa347a196
C4RLOS-JR <camjr3z@gmail.com>
2024-08-08 20:05:48 -0300

    Carlos

--- planejamento/views.py
+++ planejamento/views.py
@@ -15,9 +15,14 @@ def definir_planejamento(request):
 def modificar_valor_planejamento(request, id):
   novo_valor = json.load(request)['novo_valor']
   categoria = Categoria.objects.get(id=id)
-  
-  categoria.valor_planejamento = novo_valor
-  categoria.save()
 
-  messages.add_message(request, constants.SUCCESS, 'Valor do planejamento salvo com sucesso!')
-  return JsonResponse({'status': 'Sucesso'})
+  print(type(categoria.valor_planejamento))
+  print(type(novo_valor))
+
+  if str(categoria.valor_planejamento) != novo_valor:
+    categoria.valor_planejamento = novo_valor
+    categoria.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Valor do planejamento alterado com sucesso!')
+    return JsonResponse({'status': 'Sucesso'})
+  return JsonResponse({'status': 'Mantido'})
