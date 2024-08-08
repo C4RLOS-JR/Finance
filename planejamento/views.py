a0f0cf431b99f77ed5a76ca97f315fb4e7229faf
Leonam-Comercial <leonam.comseg@gmail.com>
2024-08-08 17:32:47 -0300

    Leonam

--- planejamento/views.py
+++ planejamento/views.py
@@ -1,12 +1,23 @@
+from email import message
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from contas.models import Categoria
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.messages import constants
+from django.contrib import messages
+import json
 
 def definir_planejamento(request):
   categorias = Categoria.objects.all()
   return render(request, 'definir_planejamento.html', {'categorias': categorias})
 
+@csrf_exempt
 def modificar_valor_planejamento(request, id):
+  novo_valor = json.load(request)['novo_valor']
   categoria = Categoria.objects.get(id=id)
+  
+  categoria.valor_planejamento = novo_valor
+  categoria.save()
 
-  return HttpResponse(categoria)
+  messages.add_message(request, constants.SUCCESS, 'Valor do planejamento alterado com sucesso')
+  return redirect('definir_planejamento')
